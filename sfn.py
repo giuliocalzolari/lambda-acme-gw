d7732d6cb088c653648a5089ccafb392608bf1bb
Giulio Calzolari <giuliocalzolari@users.noreply.github.com>
2021-09-28 14:45:17 +0200

    removed echo and die statements, lolz.

--- sfn.py
+++ sfn.py
@@ -77,39 +77,39 @@ def lambda_handler(argv, context=None):
     doms = argv.get("domains", "").split(",")
     user = argv.get("user", "glenkmurray@armyspy.com")
     base_name = "{}/ssl/{}".format(user, doms[0])
+    acme_api = True
     if "wait" in argv:
         print(f"Auto Renew process for {base_name} for domains: {doms}")
     else:
         print(f"Standard request: {base_name} for domains: {doms}")
     cert_body = s3.get_file("{}.pem".format(base_name))
-    cert = crypto.load_certificate(crypto.FILETYPE_PEM, cert_body)
-    not_after = datetime.strptime(cert.get_notAfter().decode("ascii"), "%Y%m%d%H%M%SZ")
-    now = datetime.now()
 
-    acme_api = True
-    if now > not_after:
-        msg = f"The certificate provided is expired: {not_after}, Renew!!!"
-    elif now > (not_after - timedelta(days=5)):
-        msg = f"The certificate provided is not expired yet: BUT is better to renew"
-    else:
-        # check SAN names
-        ext_count = cert.get_extension_count()
-        for i in range(ext_count):
-            ext = cert.get_extension(i)
-            if ext.get_short_name() == b'subjectAltName':
-                domain_certs = ext.__str__().replace(", ", "").split("DNS:")
-                domain_certs.pop(0)
-
-        if set(domain_certs) != set(doms):
-            msg = "SAN names mismatch... better to renew"
+    if cert_body != "": # cert does not exist
+        cert = crypto.load_certificate(crypto.FILETYPE_PEM, cert_body)
+        not_after = datetime.strptime(cert.get_notAfter().decode("ascii"), "%Y%m%d%H%M%SZ")
+        now = datetime.now()
+        if now > not_after:
+            msg = f"The certificate provided is expired: {not_after}, Renew!!!"
+        elif now > (not_after - timedelta(days=5)):
+            msg = f"The certificate provided is not expired yet: BUT is better to renew"
         else:
-            acme_api = False
-            msg = "Cert on S3 is valid and not expired"
-            key_body = s3.get_file("{}.key".format(base_name))
-            s3_cert = "s3://{}/{}".format(s3.bucket, "{}.pem".format(base_name))
-            s3_key = "s3://{}/{}".format(s3.bucket, "{}.key".format(base_name))
-            s3_csr = "s3://{}/{}".format(s3.bucket, "{}.csr".format(base_name))
-    print(msg)
+            # check SAN names
+            ext_count = cert.get_extension_count()
+            for i in range(ext_count):
+                ext = cert.get_extension(i)
+                if ext.get_short_name() == b'subjectAltName':
+                    domain_certs = ext.__str__().replace(", ", "").split("DNS:")
+                    domain_certs.pop(0)
+
+            if set(domain_certs) != set(doms):
+                msg = "SAN names mismatch... better to renew"
+            else:
+                acme_api = False
+                msg = "Cert on S3 is valid and not expired"
+                key_body = s3.get_file("{}.key".format(base_name))
+                s3_cert = "s3://{}/{}".format(s3.bucket, "{}.pem".format(base_name))
+                s3_key = "s3://{}/{}".format(s3.bucket, "{}.key".format(base_name))
+                s3_csr = "s3://{}/{}".format(s3.bucket, "{}.csr".format(base_name))
 
     if acme_api:
         s3_cert, s3_key, s3_csr, cert_body, key_body = acme_process(doms, user)
